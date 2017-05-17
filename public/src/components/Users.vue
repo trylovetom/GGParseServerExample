32f6c66c502b7ee570711d578c08529aa5fc3f13
張子晏 <try_love_tom@icloud.com>
2017-05-17 12:35:06 +0800

    retrieving current user front end

--- public/src/components/Users.vue
+++ public/src/components/Users.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="objects">
     <h1>Users({{ userObjectId }})</h1>
-    <h2>{ Session Token: "{{ sessionToken }}" }</h2>
     <h2>Session Token: "{{ sessionToken }}"</h2>
+    <h2>Current User: {{ currentUser }}</h2>
     <h2>{{ userResponse }}</h2>
     <input type="text" placeholder="username" v-model="userData.username">
     <input type="password" placeholder="password" v-model="userData.password">
     <input type="email" placeholder="email" v-model="userData.email">
     <input type="phone" placeholder="phone" v-model="userData.phone">
-    <button @click="signingUp(userData)">SigningUp</button>
-    <button @click="loggingIn(userData)">LoggingIn</button>
     <button @click="signingUp(userData)">Signing Up</button>
     <button @click="loggingIn(userData)">Logging In</button>
     <button @click="retrieveingUsers()">Retrieveing</button>
+    <button @click="retrieveingCurrentUser()">Retrieveing Current User</button>
     <!-- <button @click="retrieveingObjects()">Retrieveing</button> -->
     <!-- <button @click="updatingObjects(updateData)">Updating</button> -->
     <!-- <button @click="deletingObjects()">Deleting</button> -->
@@ -43,12 +42,14 @@ export default {
   computed: mapGetters({
     userResponse: 'userResponse',
     userObjectId: 'userObjectId',
-    sessionToken: 'sessionToken'
+    sessionToken: 'sessionToken',
+    currentUser: 'currentUser'
   }),
   methods: mapActions([
     'signingUp',
     'loggingIn',
-    'retrieveingUsers'
+    'retrieveingUsers',
+    'retrieveingCurrentUser'
   ])
 }
 </script>
